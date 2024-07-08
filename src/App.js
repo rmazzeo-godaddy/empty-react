4ae5864fe207e6cf2bd4314da06f0bb9ac7accf6
Rocky Mazzeo <rmazzeo@godaddy.com>
2024-07-08 10:49:00 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import 'h8k-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState, useEffect } from 'react';
+
+const title = "React App";
+
+// 1) create an form for first/last/email
+// 2) layout in column fashion with email taking 50% and the other fields at 25% each
+// 3) add action button
+// 4) add required validation for first last
+// 5) add email validation
+// 6) for all the below, try to create unit tests as you go
+// 7) action button should only be clickable when valid
+// 8) action button should see if a matching first/last exists in employees.js
+
+const App = () => {
+    return (
+        <div className="App">
+            <h1>page</h1>
+        </div>
+    );
 }
 
 export default App;
